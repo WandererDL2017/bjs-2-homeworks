306851e02f0d81fb8f79988f0e30f30edd7abaad
= <=>
2021-09-06 13:02:26 +0400

    3.arrays task.js edited

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,7 +2,7 @@ function compareArrays(arr1, arr2) {
   let result = arr1.length == arr2.length && arr1.every(isEqual);
 
   function isEqual(value, index) {
-        return value === arr2[index];
+    return value === arr2[index];
   }
   
   return result; // boolean
