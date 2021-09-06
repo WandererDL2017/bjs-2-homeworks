e2db2d3e05776fa15e44caec31b3b293903f9c03
= <=>
2021-09-06 12:59:20 +0400

    3.arrays/task.js solution added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.length == arr2.length && arr1.every(isEqual);
 
-  // Ваш код
-
+  function isEqual(value, index) {
+        return value === arr2[index];
+  }
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(a => a > 0 && a % 3 === 0).map( a => a * 10);
   return resultArr; // array
 }
