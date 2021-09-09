4cff42de0d4e837f8e1f7a2703b0a24064703128
= <=>
2021-09-09 14:42:37 +0400

    4.objects/task.js solution

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,30 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let tony = new Student("Tony", "male", 37);
+let buzz = new Student("Buzz", "female", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  (this.marks === undefined)? this.marks = [mark] : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function(...marks) {
+  (this.marks === undefined)? this.marks = [...marks] : this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((acum, mark) => (acum + mark)) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+} 
