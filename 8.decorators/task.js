2eb025ee48edc9b5e5786a9e1676de4f317f192f
= <=>
2021-09-23 15:20:30 +0400

    solution task8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,56 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  
+  function wrapper(...args) {
+    const hash = args.join(',');
+    const first = Object.keys(cache).splice(0, 1);
+
+    if (hash in cache) {
+      console.log("Из кэша: " + cache[hash]);
+      return "Из кэша: " + cache[hash];
+    } else {
+      let result = func(...args);
+      cache[hash] = result;
+      if (Object.keys(cache).length > 5) {
+        delete cache[first];
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
+
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let first = true;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function() {
+    if (first) {
+      first = false;
+      func();
+    }
+    
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(), ms);
+  };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timeout;
+  let first = true;
+  let count = 0;
+
+  function wrapper() {
+    count++;
+    if (first) {
+      first = false;
+      func();
+    }
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(), ms);
+  }
+  return wrapper;
 }
